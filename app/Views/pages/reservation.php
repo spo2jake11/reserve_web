ab4dc30238d363db8fb917562085545fb21123ce
Admin <spo2jake11@gmail.com>
2023-11-19 20:03:10 +0800

    Reservation form almost finish.

--- app/Views/pages/reservation.php
+++ app/Views/pages/reservation.php
@@ -15,22 +15,63 @@
             <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
         </ul>
     </header>
-    <main class="m-auto row">
-        <div class="col">
+    <main class="m-auto row d-flex container">
+        <div class="col d-flex">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula porttitor risus, at rhoncus lacus ultricies eget. Donec viverra lectus magna, et ullamcorper risus ultricies et. Sed elementum eget dui vel pretium. Maecenas dignissim pellentesque augue, a rhoncus felis lacinia vel.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula porttitor risus, 
+                at rhoncus lacus ultricies eget. Donec viverra lectus magna, et ullamcorper risus ultricies et. 
+                Sed elementum eget dui vel pretium. Maecenas dignissim pellentesque augue, a rhoncus felis lacinia vel.
             </p>
         </div>
-        <form class="col ms-5">
-            <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+        <form class="col d-flex flex-column">
+            
             <h1 class="h3 mb-3 fw-normal">Reservation Form</h1>
 
-            <div class="form-floating">
-                <input type="email" name="userEmail" class="form-control" id="floatingInput" placeholder="name@example.com" >
-                <label for="floatingInput">Email address</label>
+            <div class="form-floating my-1">
+                <input type="email" name="userEmail" class="form-control"
+                    id="floatingEmail" placeholder="name@example.com" >
+                <label for="floatingEmail">Email address</label>
             </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-            <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+
+            <div class="form-floating my-1">
+                <input type="text" name="userName" id="floatingName" class="form-control" placeholder="Name">
+                <label for="floatingName">Name</label>
+            </div>
+
+            <div class="form-floating my-1">
+                <input type="date" name="userDate" id="floatingDate" class="form-control">
+                <label for="floatingDate"></label>
+            </div>
+
+            <div class="form-floating my-1">
+                <select name="userTime" id="floatingTime" class="form-select">
+                    <option selected>Select time</option>
+                    <?php 
+                        for ($i = 15; $i <= 21; $i++)
+                        { 
+                            ?>
+                                <option value=""><?= "$i:00";?></option>
+                            <?php
+                        }
+                    ?>
+                </select>
+            </div>
+
+            <div class="form-floating">
+                <select name="userTable" id="floatingTable" class="form-select">
+                    <option selected>Expected seat/s</option>
+                    <?php 
+                        for ($i = 1; $i <= 10; $i++)
+                        {
+                            ?>
+                                <option value=""><?= $i?></option>
+                            <?php
+                        }
+                    ?>
+                </select>
+            </div>
+
+            <button class="btn btn-primary w-100 py-2" type="submit">Review Reservation</button>
         </form>
     </main>
     <style>
