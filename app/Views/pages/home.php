f14fd9dfcee0bb55894c6f1a7e749cc9d05ecccf
Admin <spo2jake11@gmail.com>
2024-01-07 21:15:30 +0800

    added homepage

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -2,6 +2,17 @@
 $this->extend('layout/main');
 $this->section('body');
 ?>
+<style>
+    article div {
+        background-image: url("/img/home.jpg");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-attachment: scroll;
+        height: 300px;
+        width: 90%;
+    }
+</style>
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <img src="<?= base_url('img/logo.png') ?>" alt="Logo" id="img_logo">
@@ -13,16 +24,43 @@ $this->section('body');
         <li class="nav-item"><a href="#" class="nav-link">About Us</a></li>
     </ul>
 </header>
-<section class="highlight">
-    <div class="row">
-        <div class="high_img col mx-auto">
-            <img src="/img/s.jpeg" alt="" class="img-fluid mx-auto">
+
+<article>
+    <div class="mx-auto d-flex justify-content-center">
+        <h2 class="text-center mx-auto my-auto h-0 bg-white">
+            Welcome to Chick n' Yats
+        </h2>
+    </div>
+</article>
+
+<section class="w-75 mx-auto">
+    <div class="row my-5">
+        <div class="col">
+            <img src="/img/1.jpg" alt="" class="w-50 d-block mx-auto">
         </div>
-        <div class="high_txt col text-center my-auto">
-            <h3>HIGHLIGHT</h3>
-            <p>kajhsdkfjhaksdjhfkahsdkjfad</p>
+        <div class="col mx-auto my-auto">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, dolor omnis? Veniam unde voluptate repudiandae inventore et recusandae iste sed impedit facere, maxime enim explicabo! Quasi ipsam possimus odio beatae.</p>
         </div>
     </div>
+    <div class="row my-5">
+        <div class="col mx-auto my-auto">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, distinctio! Itaque iure impedit voluptas quidem voluptatum quisquam ex suscipit, repudiandae fugit minus iusto praesentium. Esse praesentium doloribus necessitatibus illo provident.</p>
+        </div>
+        <div class="col">
+            <img src="/img/2.jpg" alt="" class="w-50 d-block mx-auto">
+        </div>
+    </div>
+    <div class="row my-5">
+        <div class="col">
+            <img src="/img/3.jpg" alt="" class="w-50 d-block mx-auto">
+        </div>
+        <div class="col mx-auto my-auto">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam neque deserunt obcaecati, porro adipisci dicta incidunt maiores pariatur reprehenderit nulla hic iste enim perspiciatis, nostrum dolor iusto earum illum ratione.</p>
+        </div>
+    </div>
+
+
+
 </section>
 
 
